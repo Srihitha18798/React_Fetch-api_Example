f997e5434570904dcd9601c398e3f2cd558eb362
Srihitha <srihithatsr@gmail.com>
2022-12-13 11:24:32 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data,setData]=useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos').then(
+      response=>response.json()
+    ).then(json=>setData(json))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map(item=><li key={item.id}>{item.title}</li>)}
     </div>
   );
 }
